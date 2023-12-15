36b3fb5a274780b3f104eea2e2f36178ecdc3764
BlueBojack <curtis.ducasse1g@gmail.com>
2023-12-15 16:11:23 +0100

    added ex9

--- function.cjs
+++ function.cjs
@@ -1,13 +1,33 @@
-const fs = require('fs');
+function luckyDraw(player) {
+  return new Promise((resolve, reject) => {
+    const win = Boolean(Math.round(Math.random()));
 
-const filePath = 'helloWorld.txt';
+    process.nextTick(() => {
+      if (win) {
+        resolve(`${player} won a prize in the draw!`);
+      } else {
+        reject(new Error(`${player} lost the draw.`));
+      }
+    });
+  });
+}
 
-const content = 'Hello, World!';
+const players = ['Joe', 'Caroline', 'Sabrina'];
 
-fs.writeFile(filePath, content, 'utf8', (err) => {
-  if (err) {
-    console.error('Error writing to file:', err);
-  } else {
-    console.log(`File '${filePath}' has been successfully written.`);
-  }
-});
+const drawPromises = players.map(player => {
+  return luckyDraw(player)
+    .then(res => {
+      console.log(res); 
+    })
+    .catch(err => {
+      console.error(err.message); 
+    });
+}); 
+
+Promise.all(drawPromises)
+  .then(() => {
+    console.log('All draw promises resolved.');
+  })
+  .catch(err => {
+    console.error('At least one draw promise was rejected:', err.message);
+  });
