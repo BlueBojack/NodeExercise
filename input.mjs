dc7008205ac522339d935fe972785cf72165fd3a
BlueBojack <curtis.ducasse1g@gmail.com>
2023-12-15 14:04:48 +0100

    added ex5

--- input.mjs
+++
@@ -1,5 +0,0 @@
-function isEvenNumber(n) {
-  return typeof n === 'number' && n % 2 === 0 ? 'It\'s an even number' : 'It\'s not an even number';
-}
-
-export default isEvenNumber;
--- output.mjs
+++
@@ -1,4 +0,0 @@
-import checkEvenNumber from './isEvenNumber.mjs';
-
-let input = 3;
-console.log(checkEvenNumber(input));
