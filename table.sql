b61443406709f95d8f3a169a9e2621896130ebb0
BlueBojack <curtis.ducasse1g@gmail.com>
2023-12-21 03:29:00 +0100

    added sql1

---
+++ table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Books (
+    book_id INT PRIMARY KEY,
+    title TEXT NOT NULL,
+    author TEXT NOT NULL,
+    genre TEXT,
+    published_year INT,
+    isbn VARCHAR(13) UNIQUE,
+    price DECIMAL,
+    rating FLOAT,
+    stock_count INT
+);
